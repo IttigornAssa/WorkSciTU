e298dc4f87bd4a43689b1a9ba92805c5ed99438c
ittik <boat.sb.555@gmail.com>
2018-07-01 00:00:01 +0700

    TEMPLATE

---
+++ home/static/home/uikit-3.0.0-beta.40/js/temFoot.js
@@ -0,0 +1,43 @@
+Function myFunction0() {
+    var x = document.getElementById("hide00");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById('hide0').value  = "-";
+    } else {
+        x.style.display = "none";
+        document.getElementById('hide0').value  = "+";
+    }
+}
+
+function myFunction1() {
+    var x = document.getElementById("hide11");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById('hide1').value  = "-";
+    } else {
+        x.style.display = "none";
+        document.getElementById('hide1').value  = "+";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("hide22");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById('hide2').value  = "-";
+    } else {
+        x.style.display = "none";
+        document.getElementById('hide2').value  = "+";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("hide33");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById('hide3').value  = "-";
+    } else {
+        x.style.display = "none";
+        document.getElementById('hide3').value  = "+";
+    }
+}
---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+from news.models import public,fund
+
+
+def index(request):
+    all_public = public.objects.all().order_by('-date')[:5]
+    all_fund = fund.objects.all().order_by('-date')[:5]
+    return render(request, 'home/home.html',{'all_fund': all_fund, 'all_public':all_public})
+
+
+# def index(request):
+#     return render_to_response('base.html', RequestContext(request))
+    
